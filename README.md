341e9627bf12623c42ce473024eacd857f41b65b
Brendan <brndnmcmns@gmail.com>
2016-06-14 19:14:23 -0400

    sample

--- README.md
+++ README.md
@@ -13,3 +13,5 @@
 * Good bot strategies from samples bots
  * SpinBot
  * AlienComposition
+ * TrackerBot
+ * WallBot
--- src/fnl/FnlBot.java
+++ src/fnl/FnlBot.java
@@ -11,6 +11,11 @@ package fnl;
 import robocode.AdvancedRobot;
 import robocode.Condition;
 import robocode.CustomEvent;
+import robocode.HitRobotEvent;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+import robocode.WinEvent;
+import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 import java.awt.*;
 
@@ -28,38 +33,16 @@ public class FnlBot extends AdvancedRobot {
 
 	int trigger; // Keeps track of when to move
 
-	/**
-	 * TrackFire's run method
-	 */
 	public void run() {
-		// Set colors
-		setBodyColor(new Color(0, 0, 0));
-		setGunColor(new Color(0, 0, 0));
-		setRadarColor(new Color(0, 0, 0));
 
-		// Initially, we'll move when life hits 80
-		trigger = 80;
-		// Add a custom event named "trigger hit",
-		addCustomEvent(new Condition("triggerhit") {
-			public boolean test() {
-				return (getEnergy() <= trigger);
-			}
-		});
+	}
+	
+	public void onScannedRobot(ScannedRobotEvent e) {
+		
+	}
+	
+	public void onHitRobot(HitRobotEvent e) {
+		
 	}
 
-	/**
-	 * onCustomEvent handler
-	 */
-	public void onCustomEvent(CustomEvent e) {
-		// If our custom event "triggerhit" went off,
-		if (e.getCondition().getName().equals("triggerhit")) {
-			// Adjust the trigger value, or
-			// else the event will fire again and again and again...
-			trigger -= 20;
-			out.println("Ouch, down to " + (int) (getEnergy() + .5) + " energy.");
-			// move around a bit.
-			turnLeft(65);
-			ahead(100);
-		}
-	}
 }
